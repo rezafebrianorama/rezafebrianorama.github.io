4f85b5768b5168626c9cbdf33511fa1dab6433b6
rezafebrianorama <rezafebrianorama@gmail.com>
2019-04-30 14:32:18 +0700

    up gaaan.
    new

---
+++ application/views/includes/header.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <a class="navbar-brand" href="index.html">
+            <img src="<?php echo base_url('assets/img/logo.png')?>" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="fa fa-bars"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li><a class="active" href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="causes.html">Causes</a></li>
+                <li><a href="event.html">Event</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                        Pages
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="event-details.html">Event Details</a>
+                        <a class="dropdown-item" href="elements.html">Elements</a>
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                        Blog
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="blog.html">Blog</a>
+                        <a class="dropdown-item" href="blog-details.html">Blog Details</a>
+                    </div>
+                </li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
